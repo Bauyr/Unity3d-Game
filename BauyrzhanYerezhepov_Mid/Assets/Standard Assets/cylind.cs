4b56d99d26e9128c64aea8dbac0c8a6702295c4a
Bauyr <favouriteqq@gmail.com>
2018-06-03 18:40:29 +0600

    Unity3d

---
+++ BauyrzhanYerezhepov_Mid/Assets/Standard Assets/cylind.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cylind : MonoBehaviour {
+	public float moveSpeed;
+	public Text countText;
+	private int count;
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 10f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider col){
+		
+		count = count + 1;
+		SetCountText ();
+		if (col.gameObject.CompareTag ("finish")) {
+			PrintScore ();
+
+		}
+	}
+	void SetCountText(){
+		print ("Count: "+count);
+	}
+	void PrintScore(){
+		print ("Final Score: "+count);
+	}
+
+}
---
+++ BauyrzhanYerezhepov_Mid/Assets/finishgame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finishgame : MonoBehaviour {
+
+		
+
+		void OnTriggerEnter(Collider col){
+			Destroy (this.gameObject);
+			Time.timeScale = 0;
+
+		}
+
+}
+
+
